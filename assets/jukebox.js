54d8cfefa009de2f0c89c7ae9c2a2ddcf954661c
yuejn <jennifer8ho@gmail.com>
2017-05-11 17:50:00 +0200

    initial commit

---
+++ assets/jukebox.js
@@ -0,0 +1,196 @@
+var lyrics = [
+  ["I'm just tryna get you out the friend zone", "The Hills"],
+  ["You gon' have to do it at my tempo", "The Hills"],
+  ["I'm just tryna live life for the moment", "The Hills"],
+  ["Who are you to judge, who are you to judge?", "The Hills"],
+  ["I only call you when it's half past five", "The Hills"],
+  ["I only love it when you touch me, not feel me", "The Hills"],
+
+  ["None of those toys on lease too, ah", "Starboy"],
+  ["Main bitch out of your league too, ah", "Starboy"],
+  ["House so empty, need a centerpiece", "Starboy"],
+  ["You talking 'bout me, I don't see the shade", "Starboy"],
+  ["I'm a motherfuckin' starboy", "Starboy"],
+  ["Pockets overweight, gettin' hefty, ah", "Starboy"],
+  ["Bought mama a crib and a brand new wagon", "Starboy"],
+  ["Girls get loose when they hear this song", "Starboy"],
+  ["We don't pray for love, we just pray for cars", "Starboy"],
+
+  ["Woke up by a girl, I don't even know her name", "Party Monster"],
+  ["I've been poppin', just took three in a row", "Party Monster"],
+  ["Say you're mine, I'm yours for the night", "Party Monster"],
+  ["Head be genius, dick game be the meanest", "Party Monster"],
+  ["Had to check the safe, check the dresser for my chains", "Party Monster"],
+
+  ["Bathroom stalls for the powder nose", "False Alarm"],
+  ["All red dress with the devil eyes", "False Alarm"],
+  ["So obsessed with the camera lights", "False Alarm"],
+  ["You love her, but you can't deny", "False Alarm"],
+  ["She loves everybody; she gets off all the time", "False Alarm"],
+  ["You love her but you'll never be the one", "False Alarm"],
+  ["Diamonds and the rings are her fantasy", "False Alarm"],
+  ["She chase hearts with the Hennessy", "False Alarm"],
+  ["She always seems to be alone, but the diamonds make it better", "False Alarm"],
+
+  ["Record man play my song on the radio", "Reminder"],
+  ["If it ain't XO then it gotta go", "Reminder"],
+  ["I'm like, goddamn, bitch, I am not a Teen Choice", "Reminder"],
+  ["Got a sweet Asian chick, she go Lo Mein ", "Reminder"],
+  ["Said I'm just tryna swim in something wetter than the ocean", "Reminder"],
+  ["All I wanna do is make that money and make dope shit", "Reminder"],
+  ["Platinum off a mixtape, sipping on that codeine", "Reminder"],
+  ["Rock a chain around they neck, making sure I'm geting home", "Reminder"],
+  ["And I come back to my city, I fuck every girl I know", "Reminder"],
+  ["That good sex, we'll sweat it out", "Reminder"],
+  ["Hotel bed springs, we'll wear it out", "Reminder"],
+
+  ["You don't have to spend your life with me", "Rockin'"],
+  ["We can just be rockin', yeah", "Rockin'"],
+  ["I just want your body next to me", "Rockin'"],
+  ["You see me texting, baby, you see those missed calls", "Rockin'"],
+  ["I'm actin' reckless, baby, I'm 'bout to lose it all", "Rockin'"],
+  ["This liquor got me crazy, mixed with that Adderall", "Rockin'"],
+
+  ["When it comes to money she play no games", "Six Feet Under"],
+  ["She lick it up like a candy", "Six Feet Under"],
+  ["She trying to live a life so fancy", "Six Feet Under"],
+  ["She rather die in lusting", "Six Feet Under"],
+  ["You know how she get down, pop it for a check now", "Six Feet Under"],
+  ["She don't need nobody waiting back home, she got it", "Six Feet Under"],
+  ["Real love's hard to find", "Six Feet Under"],
+
+  ["Girl, I'm just peakin', I'm just tryna be on top of you", "All I Know"],
+  ["She eat the dick up like it's an edible", "All I Know"],
+  ["When the cash overflow, let the cash overflow", "All I Know"],
+  ["Have you fucked a bitch this pretty?", "All I Know"],
+
+  ["I'm findin' ways to articulate the feeling I'm goin' through ", "Die For You"],
+  ["I just can't say I don't love you", "Die For You"],
+  ["Baby, let me tell the truth", "Die For You"],
+  ["I'm scared that I'll miss you ", "Die For You"],
+  ["I try to find a reason to pull us apart", "Die For You"],
+  ["It ain't workin', cause you're perfect", "Die For You"],
+  ["I would die for you", "Die For You"],
+  ["I'm finding ways to manipulate the feelin' you're goin' through", "Die For You"],
+  ["And you won't find no one that's better", "Die For You"],
+  ["'Cause I'm right for you, babe", "Die For You"],
+
+  ["I can feel that body shake and the heat between your legs", "I Feel It Coming"],
+  ["You've been scared of love and what it did to you", "I Feel It Coming"],
+  ["You don't have to run, I know what you've been through", "I Feel It Coming"],
+  ["Just a simple touch and it can set you free", "I Feel It Coming"],
+  ["You don't have to rush when you're alone with me ", "I Feel It Coming"],
+  ["I'm just tryna get you high ", "I Feel It Coming"],
+  ["You don't need a lonely night", "I Feel It Coming"],
+
+  ["Heaven in her mouth, got a hell of a tongue", "Ordinary Life"],
+  ["I can feel her teeth when I drive on a bump", "Ordinary Life"],
+  ["Like I'm James Dean, I'ma die when I'm young", "Ordinary Life"],
+  ["Valhalla is where all the righteous are led", "Ordinary Life"],
+  ["Devil on my lap and a cross on my neck", "Ordinary Life"],
+  ["Everybody said it would hurt in the end", "Ordinary Life"],
+  ["Angels all singin' in monasteries, yeah", "Ordinary Life"],
+
+  ["You know our love would be tragic", "Earned It"],
+
+  ["But at least we'll both be beautiful and stay forever young ", "Can't Feel My Face"],
+  ["All the misery was necessary when we're deep in love", "Can't Feel My Face"],
+
+  ["You ain't gonna catch me with them sneak pictures ", "Often"],
+  ["In my city, I'm a young god ", "Often"],
+  ["He got me redder than the devil 'til I go nauseous ", "Often"],
+  ["She asked me if I do this every day, I said, \"Often.\"", "Often"],
+  ["Make that pussy pop, and do it how I want it", "Often"],
+  ["She 'bout to go downtown for a whole hour ", "Often"],
+  ["If I had her, you can have her, man, it don't matter", "Often"],
+
+  ["You got me puttin' time in, time in ", "Acquainted"],
+  ["Nobody got me feeling this way ", "Acquainted"],
+  ["I'm used to bitches comin' right 'way", "Acquainted"],
+  ["To say that we're in love is dangerous ", "Acquainted"],
+  ["But, girl, I'm so glad we're acquainted, oh", "Acquainted"],
+  ["The fast life keeps gaining on me, shit ", "Acquainted"],
+  ["So comfort me, babe, ain't no rush from me, babe ", "Acquainted"],
+  ["These girls born in the 90s are dangerous ", "Acquainted"],
+  ["I got 'em wanting to embrace their sins", "Acquainted"],
+
+  ["Been on another level since you came, no more pain ", "House of Balloons / Glass Table Girls"],
+  ["You're in my world now, you can stay, you can stay ", "House of Balloons / Glass Table Girls"],
+  ["But you belong to me, you belong to me ", "House of Balloons / Glass Table Girls"],
+  ["If it hurts to breathe, open the window", "House of Balloons / Glass Table Girls"],
+  ["This is a happy house, we're happy here", "House of Balloons / Glass Table Girls"],
+  ["Nights pass so much quicker than the days did ", "House of Balloons / Glass Table Girls"],
+  ["And they working in the trap, so get naughty if you want ", "House of Balloons / Glass Table Girls"],
+  ["So don't blame it on me, girl, 'cause you wanted to have fun", "House of Balloons / Glass Table Girls"],
+  ["Bring the 707 out ", "House of Balloons / Glass Table Girls"],
+  ["Two puffs for the lady who be down for that ", "House of Balloons / Glass Table Girls"],
+  ["She kinda bad but she ride it like a fucking pony ", "House of Balloons / Glass Table Girls"],
+  ["If you a real man, dude, you gon' decide the truth ", "House of Balloons / Glass Table Girls"],
+  ["But I'm a nice dude with some nice dreams", "House of Balloons / Glass Table Girls"],
+  ["And we could turn this into a nightmare: Elm Street", "House of Balloons / Glass Table Girls"],
+
+  ["Bring your love, baby, I could bring my shame", "Wicked Games"],
+  ["Bring your body, baby, I could bring you fame", "Wicked Games"],
+  ["Just let me motherfucking love you", "Wicked Games"],
+  ["Get me off of this, I need confidence in myself", "Wicked Games"],
+  ["Give me all of it, I need all of it to myself", "Wicked Games"],
+
+  ["When I'm faded, forget it", "Coming Down"],
+  ["Supply what I take, I take what I spend, baby", "Coming Down"],
+  ["I always want you when I'm coming down", "Coming Down"],
+
+  ["Girl, I'm just another bird", "The Birds (part I)"],
+  ["I know you're rolling hard with it, don't lie", "The Birds (part I)"],
+
+  ["Let me fall outta love, before you fuck her, before you fuck her", "The Birds (part II)"],
+  ["Now my back hurts, she lost control", "The Birds (part II)"],
+  ["It won't be long before she falls out of love", "The Birds (part II)"],
+  ["Sandpaper kisses, papercut bliss", "The Birds (part II)"],
+
+  ["I don't know why you try to trust in me, baby, but I think I know", "Valerie"],
+
+  ["Blunts to your face, you don't wanna sing no more, lost your C notes", "XO / The Host"],
+  ["Just don't blame it on me, that you wanna come and party", "XO / The Host"],
+  ["Hipster nights downtown and your daddy don't know you're out", "XO / The Host"],
+  ["Ah yeah, are you on my cloud yet?", "XO / The Host"],
+  ["Cause I got a brand new cam, can we video feed, can we POV?", "XO / The Host"],
+  ["Oh, there's so much love to pass around", "XO / The Host"],
+  ["You said you want my heart / Well, baby, you can have it all", "XO / The Host"],
+
+  ["You knew that talking dirty to me on the phone would get me here", "Echos of Silence"],
+  ["It's gonna end how you expected, girl, you're such a masochist and I ask why", "Echos of Silence"],
+
+  ["You'll never make me stay so take your head off of me", "D.D."],
+  ["I know your every move, so won't you please let me be", "D.D."],
+  ["I've been here times before, but I was too blind to see", "D.D."],
+  ["That you seduce every man, this time you won't seduce me", "D.D."],
+  ["I have the stuff that you want / I am the thing that you need", "D.D."],
+  ["She says there's no turnin' back, she trapped me in her heart", "D.D."],
+  ["I'll be your night lovin' thing / I'll be the freak you can taunt", "D.D."],
+  ["But I hate sleepin' alone, why don't you come with me", "D.D."],
+
+  ["Just crack it, then pour it, then sip slow, then tip low", "Initiation"],
+  ["She climbing, straight to the top, forgetting why she there in the first place", "Initiation"],
+  ["No more crying, heart rate slow, put that rum down you don't wanna die tonight", "Initiation"],
+  ["There's enough to pass around, you don't gotta wait in line", "Initiation"],
+  ["Close that door before you take your fucking clothes off, baby", "Initiation"],
+  ["All I wanna do is leave 'cause I've been zoning for a week", "Initiation"],
+  ["Grab a cup of that XO, baby, I been leaning", "Initiation"],
+  ["Baby, come over here and ride it out, ride it out", "Initiation"],
+  ["Taste that lean when you kiss my mouth", "Initiation"],
+
+  ["Girl, pick up them shoes, I'll race your ass up all them stairs", "The Party & The After Party"],
+  ["Just grab a room, I swear no one will interfere", "The Party & The After Party"],
+  ["All these girls try to tell me she got no love", "The Party & The After Party"],
+  ["But all these girls never ever got her blow job", "The Party & The After Party"]
+];
+
+function hit() {
+  var random = Math.floor(Math.random() * lyrics.length);
+  document.getElementById('caption').innerHTML = lyrics[random][0];
+  document.getElementById('title').innerHTML = lyrics[random][1];
+}
+
+window.onload = function() {
+  document.getElementById('caption').value = hit();
+}
